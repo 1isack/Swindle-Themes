56dce54ebd31fe866b20352ad52f1343da59ef5e
1isack <isak02969@gmail.com>
2026-09-15 00:14:54 -0600

    Add files via upload

---
+++ config/wal/templates/swindle-colors.lua
@@ -0,0 +1,9 @@
+return {
+    root_color         = 0x{background.strip}ff,
+    inner_border_color = 0x{color4.strip}ff,
+    inner_focus_color  = 0x{color1.strip}ff,
+    inner_urgent_color = 0xff0000ff,
+    outer_border_color = 0x{background.strip}ff,
+    outer_focus_color  = 0x{background.strip}ff,
+    outer_urgent_color = 0xff0000ff,
+}
